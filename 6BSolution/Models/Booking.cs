c09842ae056c9a71dc8ae54e7a00c58e4902771f
Blake Ellis <66360391+Blake-Ellis@users.noreply.github.com>
2023-02-06 20:36:56 +0000

    Added dbcontext, going for a game of warzone

---
+++ 6BSolution/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _6BSolution.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int BookingId { get; set; }
+        public string Name { get; set; }
+        public string BookingDate { get; set; }
+        public string Flexibility { get; set; }
+        public string VehicleSize { get; set; }
+        public string ContactNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ 6BSolution/Models/CrudContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _6BSolution.Models
+{
+    public class CrudContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
