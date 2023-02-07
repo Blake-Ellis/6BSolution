9902a794935f08cd90e99eaca4554129bad3d61f
Blake Ellis <66360391+Blake-Ellis@users.noreply.github.com>
2023-02-07 22:22:48 +0000

    Finishing off the task

--- 6BSolution/Models/Booking.cs
+++ 6BSolution/Models/Booking.cs
@@ -7,10 +7,16 @@ namespace _6BSolution.Models
         [Key]
         public int BookingId { get; set; }
         public string Name { get; set; }
-        public string BookingDate { get; set; }
+        [Display(Name = "Booking Date")]
+        public DateTime BookingDate { get; set; }
         public string Flexibility { get; set; }
+        [Display(Name = "Vehicle Size")]
         public string VehicleSize { get; set; }
+        [Display(Name = "Contact Number")]
+        [Phone]
         public string ContactNumber { get; set; }
+        [Display(Name = "Email Address")]
+        [EmailAddress]
         public string EmailAddress { get; set; }
     }
 }
